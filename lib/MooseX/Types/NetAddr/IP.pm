bf3bfc4959f273db3a277f8e4f773201fda625b3
tcaine <todd.caine@gmail.com>
2012-10-30 19:34:43 -0700

    Bumped version number.

--- lib/MooseX/Types/NetAddr/IP.pm
+++ lib/MooseX/Types/NetAddr/IP.pm
@@ -3,7 +3,7 @@ package MooseX::Types::NetAddr::IP;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Class::Load 0.20 qw( load_class );
 use MooseX::Types::Moose qw/Str ArrayRef/;
